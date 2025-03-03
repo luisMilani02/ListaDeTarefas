56400cb63eff16aed9d09785a38bf05235ae9213
luis Milani <luismilani02@gmail.com>
2025-03-03 17:38:51 -0300

    Diferentes tarefas em diferentes sessoes

--- tasks/views.py
+++ tasks/views.py
@@ -1,20 +1,32 @@
 from django.shortcuts import render
 from django import forms
-
-
-tasks = ["1", '2', '3']
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 class newTaskForm(forms.Form):
     task = forms.CharField(label="New task")
-    priority = forms.IntegerField(label="priority", min_value=1, max_value=10)
+    # priority = forms.IntegerField(label="priority", min_value=1, max_value=10)
 
 # Create your views here.
 def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
     return render(request, "tasks/index.html", {
-        "tasks": tasks
+        "tasks": request.session["tasks"]
     })
 
 def add(request):
+    if request.method == "POST":
+        form = newTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            request.session["tasks"] += [task]
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+        
     return render(request, "tasks/add.html", {
         "form": newTaskForm()
     })
